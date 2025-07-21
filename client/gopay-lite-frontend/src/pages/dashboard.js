680cdb6cd57a1449e8a03ffd917d0fc0cb951a74
Ragini Sharma <raginisharma.r07@gmail.com>
2025-07-21 10:58:43 +0530

    Update dashboard.js

--- client/gopay-lite-frontend/src/pages/dashboard.js
+++ client/gopay-lite-frontend/src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import styles from '../styles/Dashboard.module.css';
+import styles from '../styles/dashboard.module.css';
 import Navbar from '../components/Navbar';
 
 export default function Dashboard() {
